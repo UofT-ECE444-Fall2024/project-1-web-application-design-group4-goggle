2efe2da677886d20b74c1edfec9c7e9df03beb3f
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-10-31 18:58:39 -0400

    incorporated error testing into Textbox component

--- frontend/uoftrade/src/components/TextBox/TextBox.tsx
+++ frontend/uoftrade/src/components/TextBox/TextBox.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { UseFormRegister, RegisterOptions, FieldValues, Path } from "react-hook-form";
+import { UseFormRegister, RegisterOptions, FieldValues, Path, FieldErrors } from "react-hook-form";
+import InlineErrorMessage from "../InlineErrorMessage/InlineErrorMessage";
 
 interface TextBoxProps<T extends FieldValues> {
   placeholder: string;
@@ -11,6 +12,7 @@ interface TextBoxProps<T extends FieldValues> {
   className?: string;
   divClassNames?: string;
   inputClassNames?: string;
+  errors?: FieldErrors<T>; // Adding errors as a prop
 }
 
 const TextBox = <T extends FieldValues>({
@@ -18,21 +20,34 @@ const TextBox = <T extends FieldValues>({
   name,
   register,
   options,
-  type="text",
+  type = "text",
   required = true,
   divClassNames = "",
   inputClassNames = "",
+  errors,
 }: TextBoxProps<T>) => {
+  // Check if there's an error for the field using "name" as the key
+  const error = errors?.[name];
+  
   return (
-    <div className={`${divClassNames} ${required ? "asterisk" : ""}`}>
-        <input
-        placeholder={placeholder}
-        type={type}
-        className={`dark:text-body-color-dark dark:shadow-two w-full rounded-xl border border-outline-grey bg-white px-6 py-3 
-                    text-base text-body-color outline-none transition-all duration-300 dark:border-transparent 
-                    dark:bg-[#2C303B] dark:focus:shadow-none ${inputClassNames}`}
-        {...(register ? register(name, { required, ...options }) : { name })}
-        />
+    <div className={`flex flex-col relative ${divClassNames}`}>
+            <input
+                placeholder={placeholder}
+                type={type}
+                className={`dark:text-body-color-dark dark:shadow-two w-full rounded-xl border border-outline-grey bg-white px-6 py-3 
+                            text-base text-body-color outline-none transition-all duration-300 dark:border-transparent 
+                            dark:bg-[#2C303B] dark:focus:shadow-none ${inputClassNames}`}
+                {...(register ? register(name, { required, ...options }) : { name })}
+            />
+            {required && <span className="asterisk"></span>}
+        {error && (
+            <div>
+            {error.type === "required" && <InlineErrorMessage message="This field is required" />}
+            {error.type === "pattern" && <InlineErrorMessage message={error.message as string} />}
+            {error.type === "validate" && <InlineErrorMessage message={error.message as string} />}
+            {/* Add more error types as needed */}
+            </div>
+        )}
     </div>
   );
 };
