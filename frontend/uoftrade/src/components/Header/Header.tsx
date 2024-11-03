17a0bff57f287d1eaaf1cccde72dd70c23566f04
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-11-03 01:26:10 -0400

    created Header component

---
+++ frontend/uoftrade/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = ({ title = "" }) => {
+  return (
+    <nav className="w-full h-[12.5vh] max-h-[110px] bg-primary flex items-center">
+      <div className="container mx-auto px-4 sm:px-8">
+        <h1
+          className="text-white-bg tracking-widest font-bold"
+          style={{ fontSize: 'min(36px, 5vh)' }}
+        >
+          {title}
+        </h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
