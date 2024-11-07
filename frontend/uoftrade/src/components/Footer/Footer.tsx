d2369d3ce235f43364912ab88edd0a828371301b
Paarth Kashyap <kashyap.paarth@gmail.com>
2024-11-07 02:09:28 -0500

    easter egg footer

--- frontend/uoftrade/src/components/Footer/Footer.tsx
+++ frontend/uoftrade/src/components/Footer/Footer.tsx
@@ -8,10 +8,9 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-white py-4">
       <div className="container mx-auto text-center">
-        <Link href="/" className="text-white text-lg font-semibold">
-          Go to Home Page
-        </Link>
+        <p className="mt-2 text-sm">Developed With Love.</p>
         <p className="mt-2 text-sm">© {new Date().getFullYear()} UofTrade. All rights reserved.</p>
+        
       </div>
     </footer>
   );
