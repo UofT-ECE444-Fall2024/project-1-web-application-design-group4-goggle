d00c5237c81acde0e03c575985f5b2720ed952e3
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-11-09 13:30:11 -0500

    made PostCard component for search results

--- frontend/uoftrade/src/app/search/[searchField]/page.tsx
+++ frontend/uoftrade/src/app/search/[searchField]/page.tsx
@@ -7,13 +7,14 @@ import Header from '@/components/Header/Header';
 import React from "react";
 import SearchSidebar from "@/components/SearchSidebar/SearchSidebar"
 import useMediaQuery from "@mui/material/useMediaQuery";
+import PostCard from '@/components/PostCard/PostCard';
 
-interface SettingsContentProps {
-  ContentComponent: React.ElementType; // Accept any React component
-  highlightIndex: number;
-}
+// interface SearchContentProps {
+//   ContentComponent: React.ElementType; // Accept any React component
+// }
 
-const CategoryPage: React.FC<SettingsContentProps> = ({ ContentComponent, highlightIndex }) => {
+// const SearchPage: React.FC<SearchContentProps> = ({ ContentComponent }) => {
+const SearchPage = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const { searchField } = useParams(); // Get the dynamic category from the URL
 
@@ -21,18 +22,37 @@ const CategoryPage: React.FC<SettingsContentProps> = ({ ContentComponent, highli
     <div className="flex flex-col min-h-screen w-full">
       <NavBar/>
       <Header title={`Search Results for \"${searchField}\"`} />
-      <div className="flex flex-col flex-grow md:flex-row">
-        <SearchSidebar highlightIndex={highlightIndex} />
-        {!isMobile && (
-          <div className="flex-grow z-30 transition-all duration-300">
-            {/* <ContentComponent /> */}
-          </div>
-        )}
-        {isMobile && <ContentComponent />}
+      <div className="flex flex-row flex-grow">
+        <SearchSidebar />
+        {/* {!isMobile && ( */}
+        <div className="flex-grow z-30 transition-all duration-300 flex justify-center ">
+          <div className='flex flex-col m-[2rem] gap-[1rem] max-w-[80%]'>
+            <PostCard
+              image="/images/misc/textbook.jpg"
+              title="Real Property Law Textbook"
+              price={150.00}
+              description="This is my description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus curabitur accumsan semper accumsan primis lorem tortor rhoncus."
+            />
+            <PostCard
+              image="/images/misc/textbook.jpg"
+              title="Real Property Law Textbook"
+              price={150.00}
+              description="This is my description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus curabitur accumsan semper accumsan primis lorem tortor rhoncus."
+            />
+            <PostCard
+              image="/images/misc/textbook.jpg"
+              title="Real Property Law Textbook"
+              price={150.00}
+              description="This is my description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus curabitur accumsan semper accumsan primis lorem tortor rhoncus."
+            />
+          </div>  
+        </div>
+        {/* )} */}
+        {/* {isMobile && <ContentComponent />} */}
       </div>
       <Footer/>
     </div>
   );
 };
 
-export default CategoryPage;
+export default SearchPage;
---
+++ frontend/uoftrade/src/components/PostCard/PostCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type PostCardProps = {
+  image: string;
+  title: string;
+  price: number;
+  description: string;
+};
+
+const PostCard: React.FC<PostCardProps> = ({ image, title, price, description }) => {
+  return (
+    <Link href="" className="border border-outline-grey rounded-lg p-4 flex space-x-4 items-center shadow-sm bg-off-white hover:bg-light-grey">
+      <div className="relative w-32 h-32 flex-shrink-0">
+        <Image
+          src={image}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-md border-outline-grey"
+        />
+      </div>
+      <div className="flex flex-col space-y-2">
+        <span className="text-xl font-bold text-primary">${price.toFixed(2)}</span>
+        <h2 className="text-lg font-semibold text-heading-1">{title}</h2>
+        <p className="text-subheading text-sm">{description}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default PostCard;
