4d271e74008ab61bd2ec4761bc69178d1ee582ed
Agrim <agrimsharma2004@gmail.com>
2024-11-09 14:00:37 -0500

    merging front end with back end stuff

--- frontend/uoftrade/src/components/Header/Header.tsx
+++ frontend/uoftrade/src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 const Header = ({ title = "" }) => {
@@ -17,23 +16,3 @@ const Header = ({ title = "" }) => {
 };
 
 export default Header;
-=======
-import React from "react";
-
-const Header = ({ title = "" }) => {
-  return (
-    <section className="w-full h-[12.5vh] max-h-[110px] bg-primary flex items-center">
-      <div className="container mx-auto px-4 sm:px-8">
-        <h1
-          className="text-white-bg tracking-widest font-bold"
-          style={{ fontSize: 'min(36px, 5vh)' }}
-        >
-          {title}
-        </h1>
-      </div>
-    </section>
-  );
-};
-
-export default Header;
->>>>>>> d60b606a78210b54a47135163c49f052c70c03bd
--- frontend/uoftrade/src/components/Settings/SettingsContent.tsx
+++ frontend/uoftrade/src/components/Settings/SettingsContent.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 
 import React from "react";
@@ -33,44 +32,3 @@ const SettingsContent: React.FC<SettingsContentProps> = ({ ContentComponent, hig
 };
 
 export default SettingsContent;
-=======
-'use client';
-
-import React from "react";
-import SettingSidebar from "@/components/SettingSidebar/SettingSidebar";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import NavBar from "../NavBar/NavBar";
-import Footer from "../Footer/Footer";
-interface SettingsContentProps {
-  ContentComponent: React.ElementType; // Accept any React component
-  highlightIndex: number;
-}
-
-/**
- * @param ContentComponent
- * Take in any react component as the content of the page and fit it with the settings sidebar  
- * @param highlightIndex
- * which menu item on the sidebar should be highlighted
- */
-const SettingsContent: React.FC<SettingsContentProps> = ({ ContentComponent, highlightIndex }) => {
-  const isMobile = useMediaQuery("(max-width: 768px)");
-
-  return (
-    <div className="flex flex-col min-h-screen w-full">
-      <NavBar/>
-      <div className="flex flex-col flex-grow md:flex-row">
-        <SettingSidebar highlightIndex={highlightIndex} />
-        {!isMobile && (
-          <div className="flex-grow z-30 transition-all duration-300">
-            <ContentComponent />
-          </div>
-        )}
-        {isMobile && <ContentComponent />}
-      </div>
-      <Footer/>
-    </div>
-  );
-};
-
-export default SettingsContent;
->>>>>>> d60b606a78210b54a47135163c49f052c70c03bd
