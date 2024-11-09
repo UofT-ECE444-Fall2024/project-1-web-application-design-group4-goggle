3a4d09865e57e217dfed45ff3101d0e9af81c0fa
jonathansanjay <33637650+jonathansanjay@users.noreply.github.com>
2024-11-09 14:27:48 -0500

    Revert "Marketplace tests"

--- marketplace_service/entrypoint.sh
+++ marketplace_service/entrypoint.sh
@@ -4,12 +4,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-# Run tests
-python manage.py test --verbosity=2
-if [ $? -ne 0 ]; then
-    echo "Tests failed. Exiting..."
-    exit 1
-fi
-
 # Start gunicorn with your application
 exec gunicorn marketplace_service.wsgi:application --bind 0.0.0.0:12001
