83534ab12afd50215562394c78655477288e8cff
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-11-09 13:54:20 -0500

    Made PostCard responsive for smaller screens

--- frontend/uoftrade/src/components/PostCard/PostCard.tsx
+++ frontend/uoftrade/src/components/PostCard/PostCard.tsx
@@ -11,8 +11,8 @@ type PostCardProps = {
 
 const PostCard: React.FC<PostCardProps> = ({ image, title, price, description }) => {
   return (
-    <Link href="" className="border border-outline-grey rounded-lg p-4 flex space-x-4 items-center shadow-sm bg-off-white hover:bg-light-grey">
-      <div className="relative w-32 h-32 flex-shrink-0">
+    <Link href="" className="border border-outline-grey rounded-lg p-4 flex flex-col md:flex-row space-x-4 items-center shadow-sm bg-off-white hover:bg-light-grey">
+      <div className="relative w-32 h-32 flex-shrink-0 ">
         <Image
           src={image}
           alt={title}
@@ -21,8 +21,8 @@ const PostCard: React.FC<PostCardProps> = ({ image, title, price, description })
           className="rounded-md border-outline-grey"
         />
       </div>
-      <div className="flex flex-col space-y-2">
-        <span className="text-xl font-bold text-primary">${price.toFixed(2)}</span>
+      <div className="flex flex-col space-y-0 md:space-y-2">
+        <span className="text-xl font-bold text-primary mt-4 md:mt-0">${price.toFixed(2)}</span>
         <h2 className="text-lg font-semibold text-heading-1">{title}</h2>
         <p className="text-subheading text-sm">{description}</p>
       </div>
