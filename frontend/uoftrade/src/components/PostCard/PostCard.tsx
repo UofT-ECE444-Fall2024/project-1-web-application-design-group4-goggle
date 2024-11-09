04a85141f7c294287e5d0a80154d8a53cb244d4c
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-11-09 14:00:00 -0500

    changed alignment in PostCard text

--- frontend/uoftrade/src/components/PostCard/PostCard.tsx
+++ frontend/uoftrade/src/components/PostCard/PostCard.tsx
@@ -11,7 +11,7 @@ type PostCardProps = {
 
 const PostCard: React.FC<PostCardProps> = ({ image, title, price, description }) => {
   return (
-    <Link href="" className="border border-outline-grey rounded-lg p-4 flex flex-col md:flex-row space-x-4 items-center shadow-sm bg-off-white hover:bg-light-grey">
+    <Link href="" className="border border-outline-grey rounded-lg p-4 flex flex-col md:flex-row space-x-4 items-center md:items-start shadow-sm bg-off-white hover:bg-light-grey">
       <div className="relative w-32 h-32 flex-shrink-0 ">
         <Image
           src={image}
@@ -21,7 +21,7 @@ const PostCard: React.FC<PostCardProps> = ({ image, title, price, description })
           className="rounded-md border-outline-grey"
         />
       </div>
-      <div className="flex flex-col space-y-0 md:space-y-2">
+      <div className="flex flex-col space-y-0 md:space-y-0 space-y-2">
         <span className="text-xl font-bold text-primary mt-4 md:mt-0">${price.toFixed(2)}</span>
         <h2 className="text-lg font-semibold text-heading-1">{title}</h2>
         <p className="text-subheading text-sm">{description}</p>
