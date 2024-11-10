1b99e6788ae0ed6b29c03af4a7b95cc89afd3797
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-11-09 20:45:02 -0500

    added value field into Textbox

--- frontend/uoftrade/src/components/TextBox/TextBox.tsx
+++ frontend/uoftrade/src/components/TextBox/TextBox.tsx
@@ -1,74 +1,71 @@
 import React from "react";
-import { UseFormRegister, RegisterOptions, FieldValues, Path, FieldErrors } from "react-hook-form";
+import { FieldValues, UseFormRegister, FieldError, Path } from "react-hook-form";
 import InlineErrorMessage from "../InlineErrorMessage/InlineErrorMessage";
-  
+
 interface TextBoxProps<T extends FieldValues> {
   placeholder: string;
   name: Path<T>;
-  register?: UseFormRegister<T>;
-  options?: RegisterOptions<T>;
-  type?: string;
-  required?: boolean;
-  className?: string;
+  register: UseFormRegister<T>;
+  errors: Record<string, FieldError | undefined>;
+  topText: string;
+  value?: string;
   divClassNames?: string;
-  inputClassNames?: string;
-  errors?: FieldErrors<T>; // Adding errors as a prop
-  topText?: string; // Text of the name above the TextBox
+  required: boolean;
+  options?: object; // For additional validation rules like pattern
   textArea?: boolean;
-  textAreaRows?: number;
+  inputClassNames?: string;
 }
 
-const TextBox = <T extends FieldValues>({
+function TextBox<T extends FieldValues>({
   placeholder,
   name,
   register,
-  options,
-  type = "text",
-  required = true,
-  divClassNames = "",
-  inputClassNames = "",
   errors,
-  topText = "",
-  textArea = false,
-  textAreaRows = 5,
-}: TextBoxProps<T>) => {
-  // Check if there's an error for the field using "name" as the key
+  topText,
+  value,
+  divClassNames,
+  required,
+  options,
+  textArea,
+  inputClassNames= '',
+}: TextBoxProps<T>) {
   const error = errors?.[name];
-  
   return (
     <div className={`mb-0 leading-none ${divClassNames}`}>
-        {topText && <span className="text-xs text-heading-1">{topText}</span>}
-        <div className="flex flex-col relative">
-                {textArea ? 
-                <textarea
-                placeholder={placeholder}
-                rows={textAreaRows}
-                className={`dark:text-body-color-dark dark:shadow-two w-full rounded-xl border border-outline-grey bg-white px-6 py-3 
-                            text-base text-body-color outline-none transition-all duration-300 dark:border-transparent 
-                            dark:bg-[#2C303B] dark:focus:shadow-none ${inputClassNames}`}
-                {...(register ? register(name, { required, ...options }) : { name })}
-            /> :
-                <input
-                    placeholder={placeholder}
-                    type={type}
-                    className={`dark:text-body-color-dark dark:shadow-two w-full rounded-xl border border-outline-grey bg-white px-6 py-3 
-                                text-base text-body-color outline-none transition-all duration-300 dark:border-transparent 
-                                dark:bg-[#2C303B] dark:focus:shadow-none ${inputClassNames}`}
-                    {...(register ? register(name, { required, ...options }) : { name })}
-                />}
-                {required && (<span className={`asterisk`}></span>)}
-            {error && (
-                <div>
-                {error.type === "required" && <InlineErrorMessage message="This field is required" />}
-                {error.type === "pattern" && <InlineErrorMessage message={error.message as string} />}
-                {error.type === "validate" && <InlineErrorMessage message={error.message as string} />}
-                {error.type === "manual" && <InlineErrorMessage message={error.message as string} />}
-                {/* Add more error types as needed */}
-                </div>
-            )}
-        </div>
+      <label htmlFor={name} className="text-xs text-heading-1">{topText}</label>
+      
+      {textArea ? (
+        <textarea
+          {...register(name, { required, ...options })}
+          placeholder={placeholder}
+          defaultValue={value}
+          className={`dark:text-body-color-dark dark:shadow-two w-full rounded-xl border border-outline-grey bg-white px-6 py-3 
+                        text-base text-body-color outline-none transition-all duration-300 dark:border-transparent 
+                        dark:bg-[#2C303B] dark:focus:shadow-none ${inputClassNames}`}
+        />
+      ) : (
+        <input
+          type="text"
+          {...register(name, { required, ...options })}
+          placeholder={placeholder}
+          defaultValue={value}
+          className={`dark:text-body-color-dark dark:shadow-two w-full rounded-xl border border-outline-grey bg-white px-6 py-3 
+                        text-base text-body-color outline-none transition-all duration-300 dark:border-transparent 
+                        dark:bg-[#2C303B] dark:focus:shadow-none ${inputClassNames}`}
+        />
+      )}
+
+      {required && <span className={`asterisk`}></span>}
+        {error && (
+          <div>
+            {error.type === "required" && <InlineErrorMessage message="This field is required" />}
+            {error.type === "pattern" && <InlineErrorMessage message={error.message as string} />}
+            {error.type === "validate" && <InlineErrorMessage message={error.message as string} />}
+            {error.type === "manual" && <InlineErrorMessage message={error.message as string} />}
+          </div>
+        )}
     </div>
   );
-};
+}
 
 export default TextBox;
