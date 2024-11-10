cdeac5cc06bec677bc8f92220cab598af5a3d226
Matthew Maciesowicz <matthewmaciesowicz@gmail.com>
2024-11-09 22:02:37 -0500

    added edit button to user's listings

--- frontend/uoftrade/src/components/Listing/ListingCard.tsx
+++ frontend/uoftrade/src/components/Listing/ListingCard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { Rating } from '@mui/material';
 import { Listing } from "@/types/listing";
+import EditIcon from '@mui/icons-material/Edit';
 
 const ListingCard = ({ listing }: { listing: Listing }) => {
   const { id, title, price, image, description, seller, tags, publishDate } = listing;
@@ -15,6 +16,9 @@ const ListingCard = ({ listing }: { listing: Listing }) => {
         >
           <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white-bg">
             {tags[0]}
+          </span>
+          <span className="absolute top-6 left-6 z-20 flex items-center justify-center p-2 bg-white rounded-full shadow-md transition-transform hover:scale-125">
+            <EditIcon className="text-primary" />
           </span>
           <Image src={image} alt="image" fill />
         </Link>
