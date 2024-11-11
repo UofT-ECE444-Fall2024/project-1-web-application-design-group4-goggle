56caaf46f4eadc4b015ff8f9e5c370f19c59813f
Sidd Topiwala <sidd.topiwala02@gmail.com>
2024-11-11 12:56:08 -0500

    refactor: remove unneeded code

--- frontend/uoftrade/src/components/ListingDetails/SellerInfo.tsx
+++ frontend/uoftrade/src/components/ListingDetails/SellerInfo.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Rating } from '@mui/material';
 import Link from 'next/link';
 import Image from 'next/image'; // Import Next.js Image component
-import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { Seller } from '@/types/seller';
 
 const SellerInfo: React.FC<{ sellerInfo: Seller | undefined, buttonLink: string, buttonText:string }> = ({ sellerInfo, buttonLink, buttonText }) => {
 
   const router = useRouter();
-
-  console.log(sellerInfo);
 
   return (
     <div className="h-full flex flex-col items-center p-4 bg-white-bg rounded-lg shadow-lg">
