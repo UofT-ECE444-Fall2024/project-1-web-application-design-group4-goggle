40316d2ab10f834630e6bf527aa1c9e1be5bbe8b
Sidd Topiwala <sidd.topiwala02@gmail.com>
2024-11-10 20:27:18 -0500

    feature: integrate home page with BE

--- frontend/uoftrade/src/app/home/page.tsx
+++ frontend/uoftrade/src/app/home/page.tsx
@@ -1,15 +1,89 @@
 'use client'
+
+import React, { useEffect, useState } from "react";
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
 import TrendingCarousel from "@/components/Carousel/TrendingCarousel";
 import CategoriesSelection from "@/components/CategoriesSelection/CategoriesSelection";
 import Loading from "@/components/Loading/Loading";
 
+import { Listing } from "@/types/listing";
+import axios from "axios";
+
 const HomePage = () => {
+  const [listings, setListings] = useState<Listing[] | Array<any>>([]);
+  const [loading, setLoading] = useState<boolean>(true); // Manage loading state for data fetching
+
+  const getProductDetails = async () => {
+    const token = localStorage.getItem('token');
+    setLoading(true);
+  
+    try {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}marketplace/products/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      // Fallback to an empty array if productsData is undefined
+      const productsData: Array<any> = response?.data?.slice(0, 5) || [];
+  
+      // Create an array of promises using .map() inside Promise.all
+      const listingsArr = await Promise.all(
+        productsData.map(async (product: any) => {
+          // Fetch user details and images asynchronously
+          const userDetails = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}identity/info/${product?.data?.user_name}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+          const userImages = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}identity/UserImages/`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            params: {
+              user_name: product?.data?.user_name,
+            },
+          });
+  
+          const sellerName = `${userDetails?.data?.firstName} ${userDetails?.data?.lastName}`;
+  
+          return {
+            id: product?.id,
+            title: product?.title,
+            price: product?.price,
+            description: product?.description,
+            images: product?.images || '',
+            location: product?.location,
+            seller: {
+              name: sellerName || 'Unknown Seller',
+              username: userImages?.data?.username || '',
+              image: userImages?.data?.images?.[0].image || '',
+              rating: userDetails?.data?.rating || 0,
+            },
+            tags: product?.category ? [product.category] : [],
+            publishDate: product?.date_posted,
+          };
+        })
+      );
+  
+      setListings(listingsArr || []);
+
+    } catch (error) {
+      console.error('Error fetching product details:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProductDetails();
+  }, [])
 
     return(
     <>
-      <Loading/>
+      <Loading loading={loading}/>
       <div className="flex flex-col min-h-screen w-full">
         {/* Header at the top */}
         <NavBar />
@@ -21,7 +95,7 @@ const HomePage = () => {
             <h1 className="text-2xl py-10 bg-primary font-bold px-10 text-left text-white-bg">
               Currently Trending on UofTrade...
             </h1>
-            <TrendingCarousel />
+            <TrendingCarousel listingData={listings}/>
           </div>
 
           {/* Categories Selection */}
